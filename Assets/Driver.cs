6130d2e5d845f68048dd3a644e04e0a167da402a
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-05-18 11:22:44 +1200

    replace translate method parameters with variables

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    float steerSpeed  = 1f;
+    float moveSpeed = 0.01f;
+
+
     void Start()
     {
 
@@ -12,7 +16,7 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-      transform.Rotate(0, 0, 0.1f);
-      transform.Translate(1, 0.01f, 0);
+      transform.Rotate(0, 0, steerSpeed);
+      transform.Translate(0, moveSpeed, 0);
     }
 }
